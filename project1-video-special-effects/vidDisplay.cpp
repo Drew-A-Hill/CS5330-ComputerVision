ab4b8fe45ec75f0c344a8279c6cfd241230ca316
Drew-A-Hill <hill.dr@northeastern.edu>
2026-05-16 16:13:10 -0400

    Update to vidDisplay

--- project1-video-special-effects/vidDisplay.cpp
+++ project1-video-special-effects/vidDisplay.cpp
@@ -9,43 +9,123 @@
 */
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
 
 using namespace cv;
 
-/*
-*/
-bool keyStrokeHandler(char key) {
-        if ( key == 'q' ) {
-            return true;
-        } else {
-            return false;
-        }
+Size getSize(VideoCapture *cap) {
+    Size refS(
+        cap->get(CAP_PROP_FRAME_WIDTH),
+        cap->get(CAP_PROP_FRAME_HEIGHT)
+    );
+
+    return refS;
 }
 
 /*
+*/
+int saveVid(VideoCapture *cap, std::string filename) {
+    int codec = VideoWriter::fourcc('M', 'J', 'P', 'G');
+    std::string filepath = filename + ".avi";
+
+    Size size = getSize(cap);
+
+    VideoWriter output;
+    output.open(filepath, codec, cap->get(CAP_PROP_FPS), size, true);
+
+    if ( !output.isOpened() ) {
+        printf("Could Not Open Output Video");
+        return -1;
+    }
+
+    Mat frame;
+
+    for (;;) {
+        *cap >> frame;
+
+        if ( frame.empty() ) {
+            break;
+        }
+        output << frame;
+
+        char key = waitKey(10);
+        if (key == 'q') {
+            break;
+        }
+    }
+    return 0;
+}
+
+// /*
+//   Handles keystrokes.
+
+//   char key the key stroke used to envoke action.
+// */
+// bool keyStrokeHandler(char key, VideoCapture *cap) {
+//         if ( key == 'q' ) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+// }
+
+// /*
+//   Displays the live video from local camera.
+
+//   VideoCapture *cap a pointer to the VideoCapture object 
+// */
+// int displayVideo(VideoCapture *cap) {
+//     Mat frame;
+//     std::string windowName = "Video";
+    
+//     namedWindow(windowName, 1);
+
+//     for(;;) {
+//         windowName = "Video1";
+//         *cap >> frame;
+
+//         if ( frame.empty() ) {
+//             printf("Frame is empty");
+//             break;
+//         }
+
+//         imshow(windowName, frame);
+
+//         char key = waitKey(10);
+//         if ( key == 'q' ) {
+//             break;
+//         } 
+//     }
+//     return 0;
+// }
+
+/*
+
   
 */
 int main(int argc, char *argv[]) {
     VideoCapture *cap;
-    Mat frame;
 
-    cap = new VideoCapture(0);
+    cap = new VideoCapture(0);                                     // Creates new video capture object and opens device
 
     if ( !cap->isOpened() ) {
         printf("Camera can not be opened!\n");
         return -1;
     }
 
-    Size refS(
-        (int) cap->get(CAP_PROP_FRAME_WIDTH),
-        (int) cap->get(CAP_PROP_FRAME_HEIGHT)
-    );
+    // Gets width and height properties of the image
+    Size size = getSize(cap);
 
-    printf("Video size: %d x %d \n", refS.height, refS.width);
-
-    namedWindow("Video", 1);
+    printf("Video size: %d x %d \n", size.height, size.width);
+    
+    Mat frame;
+    std::string windowName = "Video";
+    
+    namedWindow(windowName, 1);
 
     for(;;) {
+        windowName = "Video1";
         *cap >> frame;
 
         if ( frame.empty() ) {
@@ -53,12 +133,15 @@ int main(int argc, char *argv[]) {
             break;
         }
 
-        imshow("Video", frame);
+        imshow(windowName, frame);
 
         char key = waitKey(10);
-        if ( keyStrokeHandler(key) == true ) {
+        if ( key == 'q' ) {
             break;
-        } 
+
+        } else if (key == 's') {
+            saveVid(cap, "./Test2");
+        }
     }
 
     delete cap;
